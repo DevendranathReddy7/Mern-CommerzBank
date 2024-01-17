3322fe75d57aed31c203a734d01db07944f754c0
Devendra <devendranathreddy54@gmail.com>
2024-01-17 19:54:37 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import store from "./storeSetup/store";
+import { Provider } from "react-redux";
+
+import LoginPage from "./pages/public/LoginPage";
+import SigninPage from "./pages/public/SigninPage";
 
 const App = (props) => {
-  return <div></div>;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signin" element={<SigninPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 };
 
 export default App;
