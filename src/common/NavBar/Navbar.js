23723a53dcc93be0c4f51d17fff0279f72ae3d7f
Devendra <devendranathreddy54@gmail.com>
2024-01-21 20:55:44 +0530

    nav buttons

--- src/common/NavBar/Navbar.js
+++ src/common/NavBar/Navbar.js
@@ -6,30 +6,57 @@ import {
   TitleElemnt,
 } from "../Styles/Styles";
 import { useSelector } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
+import { DivNotifications } from "../../pages/private/HomePage/HomePageStyles";
+import NavButtons from "../Buttons/NavButtons";
 const Navbar = (props) => {
   const userName = useSelector((state) => state.login.name);
+  const navigate = useNavigate();
+  const pathName = useLocation().pathname;
   return (
-    <DivNav>
-      <div style={{ display: "inline-flex" }}>
-        <SiCommerzbank size={40} color="#0fbfeb" />
-        <div>
-          <TitleElemnt>Commerz</TitleElemnt>
+    <>
+      <DivNav>
+        <div style={{ display: "inline-flex" }}>
+          <SiCommerzbank size={40} color="#0fbfeb" />
+          <div onClick={() => navigate("/home")}>
+            <TitleElemnt>Commerz</TitleElemnt>
+          </div>
+          <div>
+            <TitleElemnt>{userName}</TitleElemnt>
+          </div>
         </div>
         <div>
-          <TitleElemnt>{userName}</TitleElemnt>
+          <ButtonStyles>🔒 Logout</ButtonStyles>
         </div>
-      </div>
-      <div>
-        <ButtonStyles>🔒 Logout</ButtonStyles>
-      </div>
-    </DivNav>
+      </DivNav>
+      {pathName === "/home" ? (
+        <DivNotifications>
+          <NavButtons to="/payments">Payments</NavButtons>
+          <NavButtons to="/settings">Settings</NavButtons>
+          <NavButtons to="/open-new-account">Open new account</NavButtons>
+        </DivNotifications>
+      ) : (
+        <>
+          <NavButtons to="/payments">Payments</NavButtons>
+          <NavButtons to="/settings">Settings</NavButtons>
+          <NavButtons to="/open-new-account">Open new account</NavButtons>
+        </>
+      )}
+    </>
   );
 };
 export default Navbar;
 
 export const Footer = () => {
   return (
-    <footer style={{ flexShrink: 0 }}>
+    <footer
+      style={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+      }}
+    >
       <DivNav>
         <FooterPara>©️ 2024 Commerz Bank. All rights reserved</FooterPara>
       </DivNav>
