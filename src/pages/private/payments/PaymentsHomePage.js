2eeab59b97f54f5af9c7d1bdfb06d035d48cd633
Devendra <devendranathreddy54@gmail.com>
2024-01-22 13:00:49 +0530

    payment & setting page

--- src/pages/private/payments/PaymentsHomePage.js
+++ src/pages/private/payments/PaymentsHomePage.js
@@ -2,11 +2,16 @@ import React from "react";
 import Navbar, { Footer } from "../../../common/NavBar/Navbar";
 import Sidebar from "../../../common/Sidebar/Sidebar";
 
+const payment = [
+  { path: "/payments/funds-transfer", name: "Funds Transfer" },
+  { path: "/payments/bill-payments", name: "Bill Payments" },
+  { path: "/payments/pay-any-one", name: "Pay Any One" },
+];
 const PaymentsHomePage = (props) => {
   return (
     <div>
       <Navbar title="Payments" />
-      <Sidebar />
+      <Sidebar to={payment} />
       <Footer />
     </div>
   );
--- src/pages/private/settings/SettingsHomePage.js
+++ src/pages/private/settings/SettingsHomePage.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar, { Footer } from "../../../common/NavBar/Navbar";
+import Sidebar from "../../../common/Sidebar/Sidebar";
 
+const settings = [
+  { path: "/settings/manage-billers", name: "Manage Billers" },
+  { path: "/settings/manage-payees", name: "Manage Payees" },
+];
 const SettingsHomePage = (props) => {
   return (
     <div>
       <Navbar title="Settings" />
+      <Sidebar to={settings} />
       <Footer />
     </div>
   );
