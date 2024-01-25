0e1d9fe765ac989f17875a5f0d57ab1b7122fc33
Devendra <devendranathreddy54@gmail.com>
2024-01-25 21:03:06 +0530

    ading outlet for settings & payments

--- src/pages/private/payments/PaymentsHomePage.js
+++ src/pages/private/payments/PaymentsHomePage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar, { Footer } from "../../../common/NavBar/Navbar";
 import Sidebar from "../../../common/Sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const PaymentsHomePage = (props) => {
   return (
     <div>
       <Navbar title="Payments" />
-      <Sidebar to="Payment" />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <Sidebar to="Payment" />
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
--- src/pages/private/settings/SettingsHomePage.js
+++ src/pages/private/settings/SettingsHomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar, { Footer } from "../../../common/NavBar/Navbar";
 import Sidebar from "../../../common/Sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const SettingsHomePage = (props) => {
   return (
     <div>
       <Navbar title="Settings" />
       <Sidebar to="Settings" />
+      <Outlet />
       <Footer />
     </div>
   );
