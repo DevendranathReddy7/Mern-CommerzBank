488dfa53fd664739789bfa2e1fe79e755e3f4039
Devendra <devendranathreddy54@gmail.com>
2024-01-28 11:14:58 +0530

    account selecting moda and review page

--- src/common/modal/AccountsModal.js
+++ src/common/modal/AccountsModal.js
@@ -1,56 +1,19 @@
 import { useState } from "react";
-import {
-  AccountListDropdown,
-  AccountsModalDiv,
-} from "../PaymentScreen/PaymentScreenStyles";
-import { Image } from "../../components/private/AccountsList/AccountsListStyles";
+import { AccountsModalDiv } from "../PaymentScreen/PaymentScreenStyles";
+import EachAccount from "./EachAccount";
 
-const AccountsModal = ({ modalOpen, acc, children }) => {
+const AccountsModal = ({ modalOpen, acc, onClick }) => {
   const [isModalOpen, setModalOpen] = useState(modalOpen);
 
-  const modelHandle = () => {
-    setModalOpen(false);
+  const modelHandle = (acc) => {
+    setModalOpen((prev) => !prev);
+    onClick(acc);
   };
   return (
     isModalOpen && (
       <AccountsModalDiv>
         {acc?.map((acc) => (
-          <AccountListDropdown onClick={modelHandle}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                gap: "10px",
-              }}
-            >
-              <div>
-                <Image
-                  src={
-                    acc.accountName
-                      ? `${process.env.PUBLIC_URL}/accounts/${acc.accountName}.png`
-                      : `${process.env.PUBLIC_URL}/accounts/cheque.png`
-                  }
-                  onError={(e) => {
-                    e.target.src = `${process.env.PUBLIC_URL}/accounts/cheque.png`;
-                  }}
-                  alt="account"
-                />
-              </div>
-              <div>
-                <span>
-                  <h4 style={{ marginBottom: "-15px" }}>{acc.accountName}</h4>
-                  <h4>{acc.accountNumber}</h4>
-                </span>
-              </div>
-            </div>
-            <div style={{ paddingRight: "10px" }}>
-              <span>
-                <h4 style={{ marginBottom: "-15px" }}>Balance</h4>
-                <h4>{acc.balance}</h4>
-              </span>
-            </div>
-          </AccountListDropdown>
+          <EachAccount acc={acc} onClick={modelHandle} />
         ))}
       </AccountsModalDiv>
     )
